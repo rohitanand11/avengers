ed68eed66e7dba61475d02a179dc62b955fef02c
Rohit Anand <rohitanand3456@gmail.com>
2020-07-05 18:21:39 +0530

    added a dataArray to be used inside the project

---
+++ src/data/dataArray/dataArray.js
@@ -0,0 +1,69 @@
+import SpiderMan from '../../assets/images/spider_man.jpg';
+import BlackPanther from '../../assets/images/black_panther.jpg';
+import AntMan from '../../assets/images/ant_man.jpg';
+import Captain from '../../assets/images/captain.jpg';
+import DoctorStrange from '../../assets/images/doctor_strange.jpg';
+import BuckyBarnes from '../../assets/images/bucky_barnes.jpg';
+import CaptainMarvel from '../../assets/images/captain_marvel.jpg';
+import Hulk from '../../assets/images/hulk.jpg';
+
+const Data = [
+    {
+        id:"spider_man_1",
+        index:0,
+        name:"spider man",
+        path:SpiderMan,
+        dialouge:"So, We are using made up names here",
+    },
+    {
+        id:"black_panther_1",
+        index:1,
+        name:"black panther",
+        path:BlackPanther,
+        dialouge:"We don't do that here :)",
+    },
+    {
+        id:"ant_man_1",
+        index:2,
+        name:"ant man",
+        path:AntMan,
+        dialouge:"I don't understand quantam physics",
+    },
+    {
+        id:"captain_1",
+        index:3,
+        name : "captain",
+        path:Captain,
+        dialouge:"I can do this all day",
+    },
+    {
+        id:"doctor_strange_1",
+        index:4,
+        name : "doctor strange",
+        path:DoctorStrange,
+        dialouge:"We are in the endgae now",
+    },
+    {
+        id:"bucky_barnes_1",
+        index:5,
+        name : "bucky barnes",
+        path:BuckyBarnes,
+        dialouge:"meet you the other side",
+    },
+    {
+        id:"captain_marvel_1",
+        index:6,
+        name : "captain marvel",
+        path:CaptainMarvel,
+        dialouge:"i am captain marvel",
+    },
+    {
+        id:"hulk_1",
+        index:7,
+        name : "hulk",
+        path:Hulk,
+        dialouge:"I hate stairs",
+    },
+]
+
+export default Data;
