1b421f6a791ab08002702d6852cbce00423b0f1e
Rohit Anand <rohitanand3456@gmail.com>
2020-08-05 01:30:02 +0530

    updated data in the custom movie list.

--- src/data/dataObj/dataObj.js
+++ src/data/dataObj/dataObj.js
@@ -90,16 +90,125 @@ const Data = {
         coloumn_names: ["movies", "year", "Character"],
         movies: [
             {
-                movie_name: "Captain America",
-                year: "2012",
+                movie_name: "Captain America: The First Avenger",
+                year: "2011",
                 main_character: "Steve Rogers",
             },
             {
-                movie_name: "iron man",
+                movie_name: "Captain Marvel",
+                year: "2019",
+                main_character: "Carol Danvers",
+            },
+            {
+                movie_name: "Iron Man",
                 year: "2008",
                 main_character: "Tony Stark",
             },
-            
+            {
+                movie_name: "Iron Man 2",
+                year: "2010",
+                main_character: "Tony Stark",
+            },
+            {
+                movie_name: "The Incredible Hulk",
+                year: "2008",
+                main_character: "Bruce Banner",
+            },
+            {
+                movie_name: "Thor",
+                year: "2011",
+                main_character: "Thor",
+            },
+            {
+                movie_name: "The Avengers ",
+                year: "2012",
+                main_character: "All",
+            },
+            {
+                movie_name: "Iron Man 3",
+                year: "2013",
+                main_character: "Tony Stark",
+            },
+            {
+                movie_name: "Thor: The Dark World",
+                year: "2013",
+                main_character: "Thor",
+            },
+            {
+                movie_name: "Captain America: Winter Soldier",
+                year: "2014",
+                main_character: "Steve Rogers",
+            },
+            {
+                movie_name: "Guardians of the Galaxy",
+                year: "2014",
+                main_character: "Rocket, Groot",
+            },
+            {
+                movie_name: "Guardians of the Galaxy Vol. 2",
+                year: "2017",
+                main_character: "Gamora ,Rocket",
+            },
+            {
+                movie_name: "Avengers: Age of Ultron",
+                year: "2015",
+                main_character: "All",
+            },
+            {
+                movie_name: "Ant-Man",
+                year: "2015",
+                main_character: "Scott Lonag",
+            },
+            {
+                movie_name: "Captain America: Civil War",
+                year: "2016",
+                main_character: "Steve Rogers",
+            },
+            {
+                movie_name: "Spider-Man: Homecoming",
+                year: "2017",
+                main_character: "Peter Parker",
+            },
+            {
+                movie_name: "Doctor Strange",
+                year: "2016",
+                main_character: "Stephen Strange",
+            },
+            {
+                movie_name: "Black Panther",
+                year: "2018",
+                main_character: "T'Challa",
+            },
+            {
+                movie_name: "Thor: Ragnarok",
+                year: "2017",
+                main_character: "Thor",
+            },
+            {
+                movie_name: "Ant-Man and The Wasp",
+                year: "2018",
+                main_character: "Scott Lang",
+            },
+            {
+                movie_name: "Black Widow",
+                year: "2020",
+                main_character: "Natasha",
+            },
+            {
+                movie_name: "Avengers: Infinity War",
+                year: "2018",
+                main_character: "All",
+            },
+            {
+                movie_name: "Avengers: Endgame",
+                year: "2019",
+                main_character: "All",
+            },
+            {
+                movie_name: "Spiderman: Far From Home",
+                year: "2019",
+                main_character: "Peter Parker",
+            },
         ]
 
     }
