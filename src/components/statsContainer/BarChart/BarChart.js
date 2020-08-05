413e4749da7a70e89559f50d8a0336674b1fcc36
Rohit Anand <rohitanand3456@gmail.com>
2020-08-06 01:39:28 +0530

    developing stats work in progress

--- src/components/statsContainer/BarChart/BarChart.js
+++ src/components/statsContainer/BarChart/BarChart.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import Classes from './BarChart.module.scss';
 
-const BarChart = ({criteriaCount,character_data}) => {
+const BarChart = ({ criteriaCount, character_data }) => {
+
     const renderEntery = () => {
-        return (
-            <div className={Classes.main_container}>
-                <div className={Classes.row}>
-                    <div className={Classes.label}>{}</div>
-                    <div className={Classes.bar}>
-                        <div className={Classes.seeds}>h</div>
-                    </div>
+
+        const Entries = [];
+
+        for (let key in character_data) {
+            Entries.push(<div className={Classes.row}>
+
+                <div className={Classes.label}>{key}</div>
+
+                <div className={Classes.bar}>
+                    {renderSeeds(character_data[key])}
+                </div>
+                <div className={Classes.score}>
+                    {character_data[key]}/10
                 </div>
             </div>
-        )
+            );
+        }
+
+        return Entries;
     };
 
+    const renderSeeds = (pScore) => {
+        const seeds = [];
+        for (let i = 0; i < pScore; i++) {
+            seeds.push(<div className={Classes.seeds}>O</div>);
+        }
+        return seeds;
+    }
+
     return (
-        <React.Fragment>
-            <div className={Classes.label}></div>
-        </React.Fragment>
+        <div className={Classes.main_container}>
+            {renderEntery()}
+        </div>
     )
 }
 
