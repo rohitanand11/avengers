8c8f1bcfa563c1e8afd212d474cbdca90b07388b
Rohit Anand <rohitanand3456@gmail.com>
2020-08-05 21:33:10 +0530

    created a new stats file

---
+++ src/components/statsContainer/stats/Stats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Classes from './Stats.module.scss';
+
+const Stats = () => {
+    return (
+        <div className={Classes.Stats}>
+            <div className={Classes.profile-picture}>
+                
+            </div>
+            <div className={Classes.pi-charts}>
+
+            </div>
+        </div>
+    );
+}
