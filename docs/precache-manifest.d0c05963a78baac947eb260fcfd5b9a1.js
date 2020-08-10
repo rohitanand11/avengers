500b72c1ff164f1411689d3aefdf106504b07ca4
Rohit Anand <rohitanand3456@gmail.com>
2020-08-11 00:27:02 +0530

    updated the build for the app

---
+++ docs/precache-manifest.d0c05963a78baac947eb260fcfd5b9a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e439bed7af957002a3e798c3bb2bbe9",
+    "url": "/avengers/index.html"
+  },
+  {
+    "revision": "6958f6e11381bba4c8be",
+    "url": "/avengers/static/css/main.a53127fe.chunk.css"
+  },
+  {
+    "revision": "c218cdd150ba1cea14b4",
+    "url": "/avengers/static/js/2.d172393e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avengers/static/js/2.d172393e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6958f6e11381bba4c8be",
+    "url": "/avengers/static/js/main.d073a015.chunk.js"
+  },
+  {
+    "revision": "b626df08463147242907",
+    "url": "/avengers/static/js/runtime-main.3ce193ee.js"
+  },
+  {
+    "revision": "a241ac8a551c26b95747050c70a15326",
+    "url": "/avengers/static/media/ant_man.a241ac8a.jpg"
+  },
+  {
+    "revision": "1194f1f7cd83e0c533d44c50903db12a",
+    "url": "/avengers/static/media/black_panther.1194f1f7.jpg"
+  },
+  {
+    "revision": "657f9991b4e62ef0bb4660745da12dc9",
+    "url": "/avengers/static/media/black_panther.657f9991.jpg"
+  },
+  {
+    "revision": "4b9ed0180e62431a629bde385f0c2fd9",
+    "url": "/avengers/static/media/black_widow.4b9ed018.jpg"
+  },
+  {
+    "revision": "de13c0b29a71b63b79fc3e37f427fef1",
+    "url": "/avengers/static/media/captain.de13c0b2.jpg"
+  },
+  {
+    "revision": "a3b0bd0ac949a3cb451e4e8474da1fc3",
+    "url": "/avengers/static/media/captain_america.a3b0bd0a.jpg"
+  },
+  {
+    "revision": "2e80fe82ff872a807dbbe0dcfad8aabb",
+    "url": "/avengers/static/media/captain_marvel.2e80fe82.jpg"
+  },
+  {
+    "revision": "d80f5cc5c1c282b14a220f84d21b4ac6",
+    "url": "/avengers/static/media/doctor_strange.d80f5cc5.jpg"
+  },
+  {
+    "revision": "44266d55f65048d0e2ab296751b13087",
+    "url": "/avengers/static/media/hulk.44266d55.jpg"
+  },
+  {
+    "revision": "e718ca289060e2f51beebdfa82700e39",
+    "url": "/avengers/static/media/hulk.e718ca28.jpg"
+  },
+  {
+    "revision": "210b7927406960e33a9de93d1965c6a3",
+    "url": "/avengers/static/media/iron_man.210b7927.jpg"
+  },
+  {
+    "revision": "2965378a32d1ae1814591fafe0700807",
+    "url": "/avengers/static/media/iron_man.2965378a.jpg"
+  },
+  {
+    "revision": "fd5ecf53d5e702bd056a508bd923fe4e",
+    "url": "/avengers/static/media/spider_man.fd5ecf53.jpg"
+  },
+  {
+    "revision": "e80719de69ab9f65a5393a54e2d8ab48",
+    "url": "/avengers/static/media/thor.e80719de.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/avengers_corousal/precache-manifest.c365efa94b05adfc4c30bb496825f579.js"
+  "/avengers/precache-manifest.d0c05963a78baac947eb260fcfd5b9a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avengers_corousal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avengers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
